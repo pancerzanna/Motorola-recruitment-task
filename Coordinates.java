7bd236af0793a1e2f2ec3551211d50c05d17259c
Anna Pancerz <58402313+pancerzanna@users.noreply.github.com>
2022-08-05 18:57:24 +0200

    Coordinates.java added

---
+++ Coordinates.java
@@ -0,0 +1,18 @@
+public class Coordinates {
+    public int x;
+    public int y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+}
